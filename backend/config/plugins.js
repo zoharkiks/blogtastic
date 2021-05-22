fa9d801e1cacf95d226ce4c3c1f8ea1672ff5bc8
zohar <zoharkiks@gmail.com>
2021-05-22 11:13:07 +0530

    Setup

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,12 +1,12 @@
-module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-    },
-    // ...
-  });
+// module.exports = ({ env }) => ({
+//     // ...
+//     upload: {
+//       provider: 'cloudinary',
+//       providerOptions: {
+//         cloud_name: env('CLOUDINARY_NAME'),
+//         api_key: env('CLOUDINARY_KEY'),
+//         api_secret: env('CLOUDINARY_SECRET'),
+//       },
+//     },
+//     // ...
+//   });
