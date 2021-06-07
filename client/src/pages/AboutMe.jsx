693a947c2a3c4d4e91fcaccf4ec9cf2da6cb6342
zohar <zoharkiks@gmail.com>
2021-06-07 16:51:36 +0530

    About Me Resp@640px

--- client/src/pages/AboutMe.jsx
+++ client/src/pages/AboutMe.jsx
@@ -4,14 +4,14 @@ const AboutMe = () => {
   return (
     <div className="aboutMe">
       <div className="bg-[#9448BC] text-[#F1DAC4] flex flex-col items-center py-4 px-5 font-montserrat space-y-5">
-        <div className=" rounded-full h-36 w-36 shadow-2xl flex items-center justify-center filter grayscale overflow-hidden ">
+        <div className=" rounded-full h-36 w-36 shadow-2xl flex items-center justify-center filter grayscale overflow-hidden sm:h-48 sm:w-48 ">
           <img
             className="object-contain "
             src="https://i.ibb.co/3yqvFDH/00100s-PORTRAIT-00100-BURST20201223233932207-COVER.jpg"
             alt=""
           />
         </div>
-        <p className="leading-loose font-medium text-xl text-left ">
+        <p className="leading-loose font-medium text-xl text-left sm:text-[1.5rem] ">
           Hi everyone, my name is Zohar Williams, creator of Blogtastic. My goal
           with Blogtastic is simple, to publish articles that are both
           entertaining 🎬 and full of insights 🧠. I really hope you enjoy the
